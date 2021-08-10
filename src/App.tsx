1ea59546d28aa2d729d2fa2cdaa50dc833beacbf
Hyoungnam Kim <kimhyoungnams@gmail.com>
2021-08-10 11:59:30 +0900

    init: :tada: initial project setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Learn React</h1>
+        <p>ji1234</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <strong>ji1234</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
